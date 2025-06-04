88d635422466169febfebd90559f80915c71f442
rsganesh <rshivaganesh@gmail.com>
2025-06-04 16:08:43 +0530

    clean yt

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,6 @@ export default defineConfig({
         "@/shortcodes/Accordion",
         "@/shortcodes/Notice",
         "@/shortcodes/Video",
-        "@/shortcodes/Youtube",
         "@/shortcodes/Tabs",
         "@/shortcodes/Tab",
       ],
